4b1fa0976d5ec5808a8635a442547ad2598a6dfe
anthonyq98 <antoqdublin@gmail.com>
2022-05-05 21:45:32 +0100

    Parsing the math.csv file using Go, printing as 2d slice

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+)
 
+func main() {
+	csvFilename := flag.String("csv", "math.csv", "a csv file for math problems")
+	flag.Parse()
+	_ = csvFilename
+
+	file, err := os.Open(*csvFilename)
+	if err != nil {
+		exit(fmt.Sprintf("Failed to open the CSV file: %s", *csvFilename))
+	}
+	
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+	if err != nil {
+		exit("Failed to parse the provided CSV file.")
+	}
+	fmt.Println(lines)
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
 }
